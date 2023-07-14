c81c64a2610227d5557265357bbf0e4457db095b
Mayhem-XD <leahblo1994@gmail.com>
2023-07-14 11:24:53 +0900

    el

---
+++ src/main/java/ch14_el_jstl/Ex01_builtInObject.java
@@ -0,0 +1,32 @@
+package ch14_el_jstl;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/ch14/el1")
+public class Ex01_builtInObject extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext ctx = request.getServletContext();
+		HttpSession sess = request.getSession();
+		ctx.setAttribute("aName", "어플리케이션 스코프");
+		sess.setAttribute("sName", "세션 스코프");
+		request.setAttribute("rName", "리퀘스트 스코프");
+		request.setAttribute("name", "리퀘스트 스코프name");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/ch14/ex02_object.jsp");
+		rd.forward(request, response);
+		
+	}
+
+
+}
